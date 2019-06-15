1c66e0847def8081fad11b4c079de314dffd57a0
german270602 <51804432+german270602@users.noreply.github.com>
2019-06-15 09:19:09 -0700

    Add files via upload

---
+++ operaciones algebra/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operaciones_algebra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Elije una de las opciones");
+            Console.WriteLine("Formula general o Binomio al cuadrado perfecto");
+            Console.WriteLine("1 o 2");
+
+            bool Op = 1;
+
+            if (Op = 1)
+            {
+
+            }
+
+
+
+        }
+    }
+}
